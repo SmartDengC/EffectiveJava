b485df965ec705d4489a7bab58413fe2c1165ae6
hahadeng <hahadeng@MacBook-Pro.local>
2024-07-21 00:58:41 +0800

    feat: 基础数据结构

--- src/main/java/org/hahadeng/learnhub/IntegerHub.java
+++ src/main/java/org/hahadeng/learnhub/IntegerHub.java
@@ -22,9 +22,30 @@ public class IntegerHub {
 		int max = Math.max(a, b);
 		int min = Math.min(a, b);
 	}
+	public static void sameValue(){
+		Integer x = new Integer(10);
+		Integer y = new Integer(10);
+		System.out.println(x.equals(y));  // compare value  -> true
+		System.out.println(x == y);  // compare place  -> false
+
+		int a = 100;
+		Integer b = 100;
+		System.out.println(a == b); // true b自动拆箱，直接和a比较值
+		Integer c = 100;
+		Integer d = 100;
+		System.out.println(c == d);  // true  自动装箱？
+		c = 200;
+		d = 200;
+		System.out.println(c == d);  // false
+		c = 127;
+		d = 127;
+		System.out.println(c == d); // true 当需要进行自动装箱时，如果数字在 -128 至 127 之间时，会直接使用缓存中的对象，而不用重新创建一个对象
+
+	}
 
 	public static void main(String[] args) {
-		IntegerHub hub  = new IntegerHub();
-		hub.temperatureTrend(new int[]{1,2}, new int[]{3,4});
+		// IntegerHub hub  = new IntegerHub();
+		// hub.temperatureTrend(new int[]{1,2}, new int[]{3,4});
+		IntegerHub.sameValue();
 	}
 }
