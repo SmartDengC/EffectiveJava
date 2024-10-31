c205c9e16d67d37a583472d230a857f8f6e7ab87
smartdengc <zero.dengc@outlook.com>
2024-10-31 10:36:37 +0800

    feat: 时间工具类

--- src/test/java/org/hahadeng/structHub/SolutionTest.java
+++ src/test/java/org/hahadeng/structHub/SolutionTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
@@ -26,5 +29,16 @@ class SolutionTest {
     void list2() {
         Solution solution = new Solution();
         solution.list2();
+    }
+
+    @Test
+    void list(){
+        Calendar calTmp = Calendar.getInstance();
+        calTmp.setTime(new Date());
+        System.out.println(
+                calTmp.get(Calendar.YEAR) + "-" + (calTmp.get(Calendar.MONTH) + 1)  + "-" + calTmp.get(Calendar.DAY_OF_MONTH)
+        );
+        String nowTime = "2024-10-31";
+
     }
 }
